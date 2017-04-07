f0de317b93b8c07f6a7dd5f3e2514725c7dde7d1
IliyaAsenov <blunt8706@gmail.com>
2017-04-07 20:10:12 +0300

    update
    new exercise html, css and Js

---
+++ exercise/test.js
@@ -0,0 +1,6 @@
+let a = [{athor:"buda"}];
+console.log(a)
+a[0].text = "asd";
+console.log(a);
+
+
--- test.js
+++
@@ -1,7 +0,0 @@
-let patt = new RegExp(/^\s+$/g);
-let text = '   ';
-if (text.match(patt)){
-    console.log('IMA')
-}else {
-    console.log('NQMA')
-}
